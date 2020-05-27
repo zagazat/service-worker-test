4ad0f4cb4536b4216ddb5e45de8b85e7182c154f
ZagitovAR <a.zagitov@modulbank.ru>
2020-05-27 19:22:54 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    async onSubmitHandler(elem) {
+        elem.preventDefault();
+        const input = document.getElementById('message');
+        const userData = {
+            message: input.value,
+        };
+        const response = await fetch('/post', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify(userData),
+        });
+        response
+            .json()
+            .then((res) => {
+                if (!response.ok) {
+                    throw res;
+                }
+                return alert(`ID: ${res.id}\nMessage: ${res.text}`);
+            })
+            .catch((error) => console.log(error));
+    }
+    render() {
+        return (
+            <form action='' id='form' onSubmit={this.onSubmitHandler}>
+                <input type='text' id='message' />
+                <button type='submit'>Send Message</button>
+            </form>
+        );
+    }
 }
 
-export default App;
+export { App };
---
+++ src/serviceWorker/custom-sw.js
@@ -0,0 +1,46 @@
+importScripts('https://unpkg.com/dexie@3.0.1/dist/dexie.js');
+importScripts('https://cdn.jsdelivr.net/npm/uuid@8.1.0/dist/umd/uuidv4.min.js');
+
+const baseUrl = self.location.origin;
+
+const database = new Dexie('testBase');
+database.version(1).stores({
+    messages: 'id, text',
+});
+
+function addResponse(responseData, status = 200) {
+    return new Response(JSON.stringify(responseData), {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        status,
+    });
+}
+
+self.addEventListener('fetch', (evt) => {
+    if (evt.request.url === `${baseUrl}/post`) {
+        const uuid = uuidv4();
+
+        evt.respondWith(
+            evt.request
+                .json()
+                .then(({ message }) => {
+                    if (message) {
+                        return database.messages.put({
+                            id: uuid,
+                            text: message,
+                        });
+                    }
+                    throw Error('oh no!');
+                })
+                .then(() => database.messages.get(uuid))
+                .then(addResponse)
+                .catch((error) => {
+                    const errorObject = {
+                        message: error.message,
+                    };
+                    return addResponse(errorObject, 400);
+                })
+        );
+    }
+});
